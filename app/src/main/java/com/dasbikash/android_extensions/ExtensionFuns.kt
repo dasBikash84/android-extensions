2ecb0f1bded7c8fec2915d264ff97068ae8fe873
Bikash Das <das.bikash.asl@gmail.com>
2020-02-27 10:57:44 +0600

    Co-routine related extension funs added.

--- app/src/main/java/com/dasbikash/android_extensions/ExtensionFuns.kt
+++ app/src/main/java/com/dasbikash/android_extensions/ExtensionFuns.kt
@@ -12,6 +12,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.text.HtmlCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 /**
  * Extension function to hide view
@@ -244,3 +250,24 @@ fun Any.dpToPx(dp: Int, context: Context): Float =
  * */
 fun Any.pxToDp(px: Int, context: Context): Float =
     (px / context.getResources().getDisplayMetrics().density)
+
+/**
+ * Extension function on launch async task
+ * suspending any suspension function
+ *
+ * @param task posted functional parameter
+ * */
+suspend fun <T:Any> runSuspended(task:()->T):T {
+    coroutineContext().let {
+        return withContext(it) {
+            return@withContext async(Dispatchers.IO) { task() }.await()
+        }
+    }
+}
+
+/**
+ * Extension function on access CoroutineContext from inside of any suspension function
+ *
+ * @return subject CoroutineContext
+ * */
+suspend fun coroutineContext(): CoroutineContext = suspendCoroutine { it.resume(it.context) }
